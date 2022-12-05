b052f3624901d1fd4278e60d61c23f0e68d4b15c
Batuhan Budak <batuhan.budak@gmail.com>
2022-12-05 15:39:18 +0300

    Add Episode component

---
+++ components/Episode.tsx
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import rickMorty from "../apis/rickMorty";
+
+export default function Episode({ id }: { id: string }) {
+  const { data, status } = useQuery(["episode", id], async () => {
+    const response = await rickMorty.get(`/episode/${id}`);
+    return response.data;
+  });
+
+  if (status !== "success") {
+    return null;
+  }
+
+  return (
+    <article key={id} className="mb-2">
+      <h6 className="text-white ">
+        {data.episode}. {data.name} - {data.air_date}
+      </h6>
+    </article>
+  );
+}
--- pages/characters/[id].tsx
+++ pages/characters/[id].tsx
@@ -9,6 +9,7 @@ type Props = {
 
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
+import Episode from "../../components/Episode";
 
 const CharacterInfoPage: NextPage<Props> = ({ characterData }) => {
   return (
@@ -38,25 +39,6 @@ const CharacterInfoPage: NextPage<Props> = ({ characterData }) => {
     </main>
   );
 };
-
-function Episode({ id }: { id: string }) {
-  const { data, status } = useQuery(["episode", id], async () => {
-    const response = await rickMorty.get(`/episode/${id}`);
-    return response.data;
-  });
-
-  if (status !== "success") {
-    return null;
-  }
-
-  return (
-    <article key={id} className="mb-2">
-      <h6 className="text-white ">
-        {data.episode}. {data.name} - {data.air_date}
-      </h6>
-    </article>
-  );
-}
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const id = context.params?.id as string;
